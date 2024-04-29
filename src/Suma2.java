59345e7178cd126b4898e43e6764a1e0610c710b
NoNiQu <marcos.workstation@gmail.com>
2024-04-29 14:01:57 +0200

    v1_1

---
+++ src/Suma2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Suma2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime que numero quieres sumar:");
+        System.out.println("Numero 1:");
+        int numero1 = sc.nextInt();
+        System.out.println("Numero 2:");
+        int numero2 = sc.nextInt();
+        String resultado = ("Hello World");
+        System.out.println("El resultado de los numeros: "+numero1+" + "+numero2 +" es: "+resultado);
+    }
+}
