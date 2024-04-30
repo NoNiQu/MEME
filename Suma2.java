f70f701e83f3f09471b80a8a8f7daf8be591597c
LauAre <148318601+LauraAre@users.noreply.github.com>
2024-04-30 08:36:34 +0200

    Add files via upload (#1)
    calabazas

---
+++ Suma2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Suma2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime que numero quieres sumar:");
+        System.out.println("Numero 2:");
+        int numero2 = sc.nextInt();
+        String resultado = ("Hello World");
+        System.out.println("El resultado de los numeros: "  + "+numero2 " + es: "+resultado");
+
+    }
+}
